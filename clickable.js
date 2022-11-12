b3a5750dd0d52905905559e94bd2643490066eb0
kociumba <kociumba1@gmail.com>
2022-11-12 16:48:40 +0100

    hosting this pile of crap

--- clickable.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById("youtube").addEventListener("click", youtube);
-
-function youtube() {
-    document.getElementById("clickable");
-    window.location.href = "https://www.youtube.com/@kociumba";
-  }
