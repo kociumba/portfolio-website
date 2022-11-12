af2672a6401d66abddaa1b365f0abea011d644ad
kociumba <kociumba1@gmail.com>
2022-11-12 13:19:02 +0100

    lot's of junk, can't be bothered to make a .gitignore

---
+++ clickable.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("youtube").addEventListener("click", youtube);
+
+function youtube() {
+    document.getElementById("clickable");
+    window.location.href = "https://www.youtube.com/@kociumba";
+  }
