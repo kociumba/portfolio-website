380139955b2fc1fbad7e470e9af561da11915a8d
kociumba <kociumba1@gmail.com>
2023-08-03 23:46:54 +0200

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-//import { initializeApp } from "./firebase/app";
-//import { getAnalytics } from "./firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//const firebaseConfig = {
-//  apiKey: "AIzaSyCjs1ByMOHbhTk-zcVrj6kUCzR7jetm5MY",
-//  authDomain: "test-site-c9999.firebaseapp.com",
-//  projectId: "test-site-c9999",
-//  storageBucket: "test-site-c9999.appspot.com",
-//  messagingSenderId: "13892104054",
-//  appId: "1:13892104054:web:dca94ff620dce5986bd457",
-//  measurementId: "G-NKFPTJPTXD"
-//};
-
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
